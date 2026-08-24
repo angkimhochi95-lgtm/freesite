0938bf9f68108538e311d5e4d8e7355ccce99e93
angkimhochi95-lgtm <angkimhochi95@gmail.com>
2026-08-24 23:59:54 +0900

    update code

--- [관리자] 깃허브_업로드.bat
+++ [관리자] 깃허브_업로드.bat
@@ -7,9 +7,9 @@ echo [1/3] 변경된 모든 파일 추가 중...
 git add .
 
 set /p commit_msg="업데이트 내용을 입력하세요 (엔터 시 기본값): "
-if "%commit_msg%"=="" set commit_msg="update code"
+if "%commit_msg%"=="" set "commit_msg=update code"
 
-echo [2/3] 커밋 생성 중: %commit_msg%
+echo [2/3] 커밋 생성 중: "%commit_msg%"
 git commit -m "%commit_msg%"
 
 echo [3/3] 깃허브 저장소로 전송 중...
